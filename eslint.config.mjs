e5debd38176d887bf62916ce3e30c7bafb56bec2
inungbp <inung.bagus@sirclo.com>
2025-12-09 20:56:25 +0700

    [9/12] - Setup typography

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,19 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  // Ignore Tailwind CSS v4 at-rules ===
+  {
+    files: ["**/*.css"],
+    rules: {
+      "css/unknownAtRules": [
+        "warn",
+        {
+          ignoreAtRules: ["theme", "utility", "layer"],
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <nav className="w-full py-4">
+            <div className="container flex justify-between items-center">
+                <div className="font-bold">Logo</div>
+                <div>Menu</div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
